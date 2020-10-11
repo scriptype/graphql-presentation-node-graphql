929a8a58e6bb34c207b6517ecee13692e36af09f
scriptype <scriptyper@gmail.com>
2020-10-11 15:21:36 +0300

    app.use(morgan).for(logging)

--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import cors from 'cors';
+import morgan from 'morgan';
 import dotenv from 'dotenv';
 dotenv.config();
 
 import MySchema from './schema';
 
 const app = express();
+
+app.use(morgan('common'));
 
 app.use(
   '/graphql',
